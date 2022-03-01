dc2b1528b7e232a23e837da1e0e5d55aafb0e20c
Russel Claveria <rmclaveria@iskolarngbayan.pup.edu.ph>
2022-03-01 21:42:16 +0800

    add accessors

--- app/Models/Borrowing.php
+++ app/Models/Borrowing.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Borrowing extends Model
 {
@@ -10,7 +11,7 @@ class Borrowing extends Model
     protected $primaryKey = 'id';
     protected $guarded = [];
     public $timestamps = true;
-    protected $appends = ['formatted_date_borrowed_returned'];
+    protected $appends = ['formatted_date_borrowed_returned', 'formatted_date_borrowed', 'formatted_date_returned', 'days_overdue'];
     
     public function materialCopy()
     {
@@ -31,5 +32,34 @@ class Borrowing extends Model
         return date_format(date_create($this->date_borrowed), 'F d, Y') 
             . ' - '
             . date_format(date_create($this->date_returned), 'F d, Y') ;
+    }
+
+    /**
+     * Format the Date Borrowed Column
+     * @return string
+     */
+    public function getFormattedDateBorrowedAttribute()
+    {
+        return date_format(date_create($this->date_borrowed), 'F d, Y');
+    }
+
+    /**
+     * Format the Date Returned Column
+     * @return string
+     */
+    public function getFormattedDateReturnedAttribute()
+    {
+        return date_format(date_create($this->date_returned), 'F d, Y');
+    }
+
+    /**
+     * Get the Overdue Date of Borrowing
+     * @return int
+     */
+    public function getDaysOverdueAttribute(): int
+    {
+        $returned_date = Carbon::createMidnightDate($this->date_returned);
+        $daysOverdue = (int) $returned_date->diffInDays();
+        return $daysOverdue;
     }
 }
