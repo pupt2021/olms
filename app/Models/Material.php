f167d565fc70a7daee51a9aeead05657f050d80b
Russel Claveria <rmclaveria@iskolarngbayan.pup.edu.ph>
2022-03-03 05:18:34 +0800

    add softdeletes on model

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
+    
 class Material extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'materials';
     protected $primaryKey = 'materials_id';
     protected $guarded = [];
